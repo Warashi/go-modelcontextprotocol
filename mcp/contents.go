c90dafb09aaf4455acb4c2c088815a54ed87a803
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-03-19 18:11:34 +0900

    refactor: move content types to contents.go
    Signed-off-by: Shinnosuke Sawada-Dazai <shin@warashi.dev>

---
+++ mcp/contents.go
@@ -0,0 +1,64 @@
+package mcp
+
+import (
+	"encoding/base64"
+	"encoding/json"
+)
+
+// isContent is an interface for the content of the tool call result.
+type isContent interface {
+	isContent()
+}
+
+// TextContent is the text content of the tool call result.
+// TODO: add Annotations field
+type TextContent struct {
+	Text string `json:"text"`
+}
+
+// isContent implements isContent.
+func (TextContent) isContent() {}
+
+// MarshalJSON implements json.Marshaler.
+func (t *TextContent) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]any{
+		"type": "text",
+		"text": t.Text,
+	})
+}
+
+// ImageContent is the image content of the tool call result.
+// TODO: add Annotations field
+type ImageContent struct {
+	Data     []byte `json:"data"`
+	MimeType string `json:"mimeType"`
+}
+
+// isContent implements isContent.
+func (ImageContent) isContent() {}
+
+// MarshalJSON implements json.Marshaler.
+func (t *ImageContent) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]any{
+		"type":     "image",
+		"data":     base64.StdEncoding.EncodeToString(t.Data),
+		"mimeType": t.MimeType,
+	})
+}
+
+// EmbeddedResource is the embedded resource content of the tool call result.
+// TODO: add Annotations field
+type EmbeddedResource struct {
+	Resource Resource `json:"resource"`
+}
+
+// isContent implements isContent.
+func (EmbeddedResource) isContent() {}
+
+// MarshalJSON implements json.Marshaler.
+func (t *EmbeddedResource) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]any{
+		"type":     "resource",
+		"resource": t.Resource,
+	})
+}
--- mcp/tools.go
+++ mcp/tools.go
@@ -3,7 +3,6 @@ package mcp
 import (
 	"context"
 	"encoding"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"maps"
@@ -100,64 +99,6 @@ type ToolHandlerFunc[Input any] func(ctx context.Context, input Input) ([]any, e
 // Handle implements ToolHandler.
 func (f ToolHandlerFunc[Input]) Handle(ctx context.Context, input Input) ([]any, error) {
 	return f(ctx, input)
-}
-
-// isContent is an interface for the content of the tool call result.
-type isContent interface {
-	isContent()
-}
-
-// TextContent is the text content of the tool call result.
-// TODO: add Annotations field
-type TextContent struct {
-	Text string `json:"text"`
-}
-
-// isContent implements isContent.
-func (TextContent) isContent() {}
-
-// MarshalJSON implements json.Marshaler.
-func (t *TextContent) MarshalJSON() ([]byte, error) {
-	return json.Marshal(map[string]any{
-		"type": "text",
-		"text": t.Text,
-	})
-}
-
-// ImageContent is the image content of the tool call result.
-// TODO: add Annotations field
-type ImageContent struct {
-	Data     []byte `json:"data"`
-	MimeType string `json:"mimeType"`
-}
-
-// isContent implements isContent.
-func (ImageContent) isContent() {}
-
-// MarshalJSON implements json.Marshaler.
-func (t *ImageContent) MarshalJSON() ([]byte, error) {
-	return json.Marshal(map[string]any{
-		"type":     "image",
-		"data":     base64.StdEncoding.EncodeToString(t.Data),
-		"mimeType": t.MimeType,
-	})
-}
-
-// EmbeddedResource is the embedded resource content of the tool call result.
-// TODO: add Annotations field
-type EmbeddedResource struct {
-	Resource Resource `json:"resource"`
-}
-
-// isContent implements isContent.
-func (EmbeddedResource) isContent() {}
-
-// MarshalJSON implements json.Marshaler.
-func (t *EmbeddedResource) MarshalJSON() ([]byte, error) {
-	return json.Marshal(map[string]any{
-		"type":     "resource",
-		"resource": t.Resource,
-	})
 }
 
 // Handle handles the tool call.
